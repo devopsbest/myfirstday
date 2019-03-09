1ac8115e578bb10d8f660f7ebeba157dcf0425d7
devopsbest <lucklly@163.com>
2019-03-09 22:26:17 +0800

    update the login

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template,flash,request
+from flask import Flask, render_template,flash,request,redirect,url_for
 # 引入Form基类
 from flask_wtf import Form
 # 引入Form元素父类
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField,SubmitField
 # 引入Form验证父类
 from wtforms.validators import DataRequired
 
@@ -13,27 +13,37 @@ app.config["SECRET_KEY"] = "12345678"
 
 class LoginForm(Form):
     # 用户名
-    username = StringField('username', validators=[DataRequired(message="用户名不能为空")])
+    username = StringField('用户名:', validators=[DataRequired(message="用户名不能为空")])
     # 密码
-    password = PasswordField('password', validators=[DataRequired(message="密码不能为空")])
+    password = PasswordField('密码:', validators=[DataRequired(message="密码不能为空")])
+
+    submit = SubmitField('提交')
 
 
 @app.route("/index", methods=['GET', 'POST'])
 def baselogin():
-    form = LoginForm()
+    login_form = LoginForm()
+    # 逻辑处理
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        # 判断是否是验证提交
-        if username =="user" and password=="password":
-            # 跳转
-            flash(form.username.data + '|' + form.password.data)
-            return render_template('home.html',username=username)
-        else:
-            message = "Failed Login"
-            return render_template('index.html',message=message)
 
-    return render_template('index.html')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        # 判断是否是验证提交
+        if login_form.validate_on_submit():
+            if username =="user" and password=="password":
+                # 跳转
+                message = "welcome user {}".format(login_form.username.data)
+                return render_template('home.html',message=message)
+            else:
+                message = "Failed Login"
+                return render_template('index.html',form=login_form,message=message)
+
+    return render_template('index.html',form=login_form)
+
+
+@app.route("/home", methods=['GET', 'POST'])
+def home():
+    return render_template("home.html")
 
 
 if __name__ == '__main__':
